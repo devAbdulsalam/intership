81ecf2088f0e0594e3333a9fc19b2119d3b245e5
my-amtech <ammuftau74@gmail.com>
2024-01-02 23:30:17 +0100

    First commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,13 @@
+// import { useEffect } from 'react';
 import stemImage from '../assets/stem2.png';
+import { Link } from 'react-router-dom';
 const Home = () => {
+	// const navigate = useNavigate();
+	// useEffect(() => {
+	// 	setTimeout(() => {
+	// 		navigate('/register');
+	// 	}, 1000);
+	// }, [navigate]);
 	return (
 		<div
 			className="relative min-h-screen flex items-center justify-center bg-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8 bg-no-repeat bg-cover"
@@ -8,6 +16,8 @@ const Home = () => {
 			}}
 		>
 			<div className="absolute bg-black opacity-60 inset-0 z-0"></div>
+			<h2>WelCome</h2>
+			<Link to="/register">Register</Link>
 		</div>
 	);
 };
