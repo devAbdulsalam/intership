f4ad245549c1a2a389588ce1bcf1b017d3c92e9e
my-amtech <ammuftau74@gmail.com>
2024-01-08 16:56:16 +0100

    reduce image opacity

--- src/components/LeftImage.jsx
+++ src/components/LeftImage.jsx
@@ -1,27 +1,26 @@
-import Logo from './../assets/Stem2.png';
 import Social from './Social';
 import { motion } from 'framer-motion';
 const LeftImage = () => {
 	return (
 		<div className="lg:flex w-1/2 hidden relative items-center bgImage bg-cover bg-no-repeat">
 			<div className="absolute bg-primary/50 opacity-60 inset-0 z-0"></div>
-			<div className="w-full px-24 z-10 text-center text-white opacity-70">
-				<motion.div
-					initial={{ opacity: 0, scale: 0.5 }}
-					animate={{ opacity: 1, scale: 1 }}
-					transition={{ duration: 0.8, delay: 0.5 }}
-				>
-					<div className="flex items-center justify-center w-full">
-						<img className="w-32" src={Logo} alt="" />
-					</div>
-				</motion.div>
+			<div className="w-full px-24 z-10 text-center text-white">
 				<motion.div
 					initial={{ x: 50 }}
 					animate={{ x: 0 }}
-					transition={{ ease: 'easeOut', duration: 2, delay: 0.7 }}
+					transition={{ ease: 'easeOut', duration: 1, delay: 0.5 }}
 				>
-					<h2 className="mt-2 text-2xl tracking-wide font-bold">
-						Build, Launch and Scale Innovations
+					<h2 className="mt-2 text-4xl tracking-wide font-bold">
+						Build, Launch and Scale
+					</h2>
+				</motion.div>
+				<motion.div
+					initial={{ x: -50 }}
+					animate={{ x: 0 }}
+					transition={{ ease: 'easeOut', duration: 1, delay: 0.5 }}
+				>
+					<h2 className="mt-4 text-4xl tracking-wide font-semibold">
+						Innovations
 					</h2>
 				</motion.div>
 			</div>
