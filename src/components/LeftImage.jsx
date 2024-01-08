d1d4f6cd660de4cffb9ae44ce7472ccac7e91948
my-amtech <ammuftau74@gmail.com>
2024-01-08 15:49:54 +0100

    reduce image opacity

--- src/components/LeftImage.jsx
+++ src/components/LeftImage.jsx
@@ -3,9 +3,9 @@ import Social from './Social';
 import { motion } from 'framer-motion';
 const LeftImage = () => {
 	return (
-		<div className="lg:flex w-1/2 hidden relative items-center">
-			<div className="absolute bg-primary/30 opacity-60 inset-0 z-0"></div>
-			<div className="w-full px-24 z-10 text-center text-primary">
+		<div className="lg:flex w-1/2 hidden relative items-center bgImage bg-cover">
+			<div className="absolute bg-primary/80 opacity-60 inset-0 z-0"></div>
+			<div className="w-full px-24 z-10 text-center text-white opacity-50">
 				<motion.div
 					initial={{ opacity: 0, scale: 0.5 }}
 					animate={{ opacity: 1, scale: 1 }}
@@ -20,10 +20,9 @@ const LeftImage = () => {
 					animate={{ x: 0 }}
 					transition={{ ease: 'easeOut', duration: 2, delay: 0.7 }}
 				>
-					<p className="text-2xl mt-4 font-semibold text-primary-light">
-						Build, Launch and Scale
-					</p>
-					<h2 className=" text-5xl tracking-wide font-bold">Innovation</h2>
+					<h2 className="mt-2 text-2xl tracking-wide font-bold">
+						Build, Launch and Scale Innovations
+					</h2>
 				</motion.div>
 			</div>
 			<div className="bottom-0 absolute p-4 text-center right-0 left-0 flex justify-center space-x-4">
